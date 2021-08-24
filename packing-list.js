7482a3e54bb3459d672a97452e35d5bdf328d318
tonyloba <tonyloba@gmail.com>
2021-08-24 18:33:50 +0000

    added for and while loop

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+//FOR LOOP:
+
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+//WHILE LOOP:
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
 
