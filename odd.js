4c1b9f1c5e31f1cd939e7d1606e2204815e1ae98
tonyloba <tonyloba@gmail.com>
2021-08-24 00:11:26 +0000

    added odd or not function

---
+++ odd.js
@@ -0,0 +1,9 @@
+// Task : Create a function to determine if a number is odd or not.
+function isOdd(num) {
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isOdd(4));
